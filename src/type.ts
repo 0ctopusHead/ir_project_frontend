18099f6fc6967f4802726fd21fe69348477ed85f
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-03-15 16:58:01 +0700

    create the interface to get the response from backend

---
+++ src/type.ts
@@ -0,0 +1,18 @@
+export interface QueryItems {
+  corrected_query: string
+  results: QueryResults
+}
+export interface QueryResults {
+  AggregatedRatings: number
+  AuthorId: number
+  AuthorName: string
+  Description: string
+  Images: string
+  Name: string
+  RecipeCategory: string
+  RecipeId: number
+  RecipeIngredientParts: string
+  RecipeInstructions: string
+  ReviewCount: number
+  score: number
+}
