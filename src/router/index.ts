778df591297a9972bc7ca69bf726a98436226584
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-03-16 05:32:52 +0700

    installl the dependencies

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import RecipeListView from '@/views/RecipeListView.vue'
+import RecipeDetail from '@/views/RecipeDetail.vue'
+import { useRecipeStore } from '@/stores/recipe'
+import QueryService from '@/services/QueryService'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +19,23 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/recipe-search',
+      name: 'recipe',
+      component: RecipeListView
+    },
+    {
+      path: '/recipe/:id',
+      name: 'recipe-detail',
+      component: RecipeDetail,
+      props: true,
+      beforeEnter: (to) => {
+        const id = parseInt(to.params.id as string)
+        const recipeStore = useRecipeStore()
+        recipeStore.getRecipeById(id)
+        console.log(recipeStore)
+      }
     }
   ]
 })
---
+++ src/stores/recipe.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+import type { QueryResults } from '@/type'
+
+export const useRecipeStore = defineStore('recipe', {
+  state: () => ({
+    recipe: null as QueryResults | null,
+    recipes: [] as QueryResults[]
+  }),
+  actions: {
+    setRecipes(recipes: QueryResults[]) {
+      this.recipes = recipes
+    },
+    getRecipeById(recipeId: number) {
+      this.recipe = this.recipes.find((recipe) => recipe.RecipeId === recipeId) || null
+    }
+  }
+})
